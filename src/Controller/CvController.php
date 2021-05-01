0c3b6514dbc4b5d4ad50fcb71532119e05d881b9
emnaboukhris <79046370+emnaboukhris@users.noreply.github.com>
2021-05-01 03:04:15 +0200

    first commit

---
+++ src/Controller/CvController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CvController extends AbstractController
+{
+    /**
+     * @Route("/cv", name="cv")
+     */
+    public function index(): Response
+    {
+        return $this->render('cv/index.html.twig', [
+            'controller_name' => 'CvController',
+        ]);
+    }
+    /**
+     * @Route("/cv/{name}/{firstname}/{age}/{section}")
+     */
+
+
+
+
+
+
+
+}
